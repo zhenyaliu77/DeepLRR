e48d83077ae5e19b9ad82b9aa5ebd537bde7ac84
Zirui Ren <alexrenzr@gmail.com>
2021-07-25 03:36:40 +0800

    Update software package

--- DeepLRR-pipeline/auto_deploy.sh
+++ DeepLRR-pipeline/auto_deploy.sh
@@ -8,5 +8,6 @@ echo -e "#SignalP path\nexport PATH=\$PATH:"$PWD"/tools/signalp-5.0b/bin:"$PWD"/
 echo -e "#TMHMM path\nexport PATH=\$PATH:"$PWD"/tools/tmhmm-2.0c/bin\n" >>~/.bashrc
 echo -e "export PERL5LIB=\$PERL5LIB:"$PWD"/tools/PfamScan/Bio/Pfam/Scan\n" >>~/.bashrc
 echo -e "export PERL5LIB=\$PERL5LIB:"$PWD"/tools/PfamScan\n" >> ~/.bashrc
-
+echo -e "#COILS path\nexport PATH=\$PATH:"$PWD"/tools/coils\n" >>~/.bashrc
+echo -e "#COILSDIR path\nexport COILSDIR="$PWD"/tools/coils\n" >>~/.bashrc
 echo "Then input 'source ~/.bashrc' in the console to finish deployment!"
